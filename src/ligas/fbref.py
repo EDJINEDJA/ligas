6630ec6d1d2d2bd8a5b3e9d9f54800ad871c5da9
ArafatePro <91158220+ArafatePro@users.noreply.github.com>
2024-08-19 17:19:01 +0200

    links for differents leagues

--- src/ligas/fbref.py
+++ src/ligas/fbref.py
@@ -56,6 +56,132 @@ browser_headers = {
     }
 }
 
+
+ligaslinks = {
+    # Men's club international cups
+    'Copa Libertadores': {
+        'history url': 'https://fbref.com/en/comps/14/history/Copa-Libertadores-Seasons',
+        'finders': ['Copa-Libertadores']},
+    'Champions League': {
+        'history url': 'https://fbref.com/en/comps/8/history/Champions-League-Seasons',
+        'finders': ['European-Cup', 'Champions-League']},
+    'Europa League': {
+        'history url': 'https://fbref.com/en/comps/19/history/Europa-League-Seasons',
+        'finders': ['UEFA-Cup', 'Europa-League']},
+    'Europa Conference League': {
+        'history url': 'https://fbref.com/en/comps/882/history/Europa-Conference-League-Seasons',
+        'finders': ['Europa-Conference-League']},
+    # Men's national team competitions
+    'World Cup': {
+        'history url': 'https://fbref.com/en/comps/1/history/World-Cup-Seasons',
+        'finders': ['World-Cup']},
+    'Copa America': {
+        'history url': 'https://fbref.com/en/comps/685/history/Copa-America-Seasons',
+        'finders': ['Copa-America']},
+    'Euros': {
+        'history url': 'https://fbref.com/en/comps/676/history/European-Championship-Seasons',
+        'finders': ['UEFA-Euro', 'European-Championship']},
+    # Men's big 5
+    'Big 5 combined': {
+        'history url': 'https://fbref.com/en/comps/Big5/history/Big-5-European-Leagues-Seasons',
+        'finders': ['Big-5-European-Leagues']},
+    'EPL': {
+        'history url': 'https://fbref.com/en/comps/9/history/Premier-League-Seasons',
+        'finders': ['Premier-League', 'First-Division']},
+    'Ligue 1': {
+        'history url': 'https://fbref.com/en/comps/13/history/Ligue-1-Seasons',
+        'finders': ['Ligue-1', 'Division-1']},
+    'Bundesliga': {
+        'history url': 'https://fbref.com/en/comps/20/history/Bundesliga-Seasons',
+        'finders': ['Bundesliga']},
+    'Serie A': {
+        'history url': 'https://fbref.com/en/comps/11/history/Serie-A-Seasons',
+        'finders': ['Serie-A']},
+    'La Liga': {
+        'history url': 'https://fbref.com/en/comps/12/history/La-Liga-Seasons',
+        'finders': ['La-Liga']},
+    # Men's domestic leagues - 1st tier
+    'MLS': {
+        'history url': 'https://fbref.com/en/comps/22/history/Major-League-Soccer-Seasons',
+        'finders': ['Major-League-Soccer']},
+    'Brazilian Serie A': {
+        'history url': 'https://fbref.com/en/comps/24/history/Serie-A-Seasons',
+        'finders': ['Serie-A']},
+    'Eredivisie': {
+        'history url': 'https://fbref.com/en/comps/23/history/Eredivisie-Seasons',
+        'finders': ['Eredivisie']},
+    'Liga MX': {
+        'history url': 'https://fbref.com/en/comps/31/history/Liga-MX-Seasons',
+        'finders': ['Primera-Division', 'Liga-MX']},
+    'Primeira Liga': {
+        'history url': 'https://fbref.com/en/comps/32/history/Primeira-Liga-Seasons',
+        'finders': ['Primeira-Liga']},
+    'Belgian Pro League': {
+        'history url': 'https://fbref.com/en/comps/37/history/Belgian-Pro-League-Seasons',
+        'finders': ['Belgian-Pro-League', 'Belgian-First-Division']},
+    'Argentina Liga Profesional': {
+        'history url': 'https://fbref.com/en/comps/21/history/Primera-Division-Seasons',
+        'finders': ['Primera-Division']},
+    # Men's domestic league - 2nd tier
+    'EFL Championship': {
+        'history url': 'https://fbref.com/en/comps/10/history/Championship-Seasons',
+        'finders': ['First-Division', 'Championship']},
+    'La Liga 2': {
+        'history url': 'https://fbref.com/en/comps/17/history/Segunda-Division-Seasons',
+        'finders': ['Segunda-Division']},
+    '2. Bundesliga': {
+        'history url': 'https://fbref.com/en/comps/33/history/2-Bundesliga-Seasons',
+        'finders': ['2-Bundesliga']},
+    'Ligue 2': {
+        'history url': 'https://fbref.com/en/comps/60/history/Ligue-2-Seasons',
+        'finders': ['Ligue-2']},
+    'Serie B': {
+        'history url': 'https://fbref.com/en/comps/18/history/Serie-B-Seasons',
+        'finders': ['Serie-B']},
+    # Women's internation club competitions
+    'Womens Champions League': {
+        'history url': 'https://fbref.com/en/comps/181/history/Champions-League-Seasons',
+        'finders': ['Champions-League']},
+    # Women's national team competitions
+    'Womens World Cup': {
+        'history url': 'https://fbref.com/en/comps/106/history/Womens-World-Cup-Seasons',
+        'finders': ['Womens-World-Cup']},
+    'Womens Euros': {
+        'history url': 'https://fbref.com/en/comps/162/history/UEFA-Womens-Euro-Seasons',
+        'finders': ['UEFA-Womens-Euro']},
+    # Women's domestic leagues
+    'NWSL': {
+        'history url': 'https://fbref.com/en/comps/182/history/NWSL-Seasons',
+        'finders': ['NWSL']},
+    'A-League Women': {
+        'history url': 'https://fbref.com/en/comps/196/history/A-League-Women-Seasons',
+        'finders': ['A-League-Women', 'W-League']},
+    'WSL': {
+        'history url': 'https://fbref.com/en/comps/189/history/Womens-Super-League-Seasons',
+        'finders': ['Womens-Super-League']},
+    'D1 Feminine': {
+        'history url': 'https://fbref.com/en/comps/193/history/Division-1-Feminine-Seasons',
+        'finders': ['Division-1-Feminine']},
+    'Womens Bundesliga': {
+        'history url': 'https://fbref.com/en/comps/183/history/Frauen-Bundesliga-Seasons',
+        'finders': ['Frauen-Bundesliga']},
+    'Womens Serie A': {
+        'history url': 'https://fbref.com/en/comps/208/history/Serie-A-Seasons',
+        'finders': ['Serie-A']},
+    'Liga F': {
+        'history url': 'https://fbref.com/en/comps/230/history/Liga-F-Seasons',
+        'finders': ['Liga-F']},
+    # Women's domestic cups
+    'NWSL Challenge Cup': {
+        'history url': 'https://fbref.com/en/comps/881/history/NWSL-Challenge-Cup-Seasons',
+        'finders': ['NWSL-Challenge-Cup']},
+    'NWSL Fall Series': {
+        'history url': 'https://fbref.com/en/comps/884/history/NWSL-Fall-Series-Seasons',
+        'finders': ['NWSL-Fall-Series']},
+}
+
+
+
 class fbref():
     def __init__(self, wait_time :int =10) -> None:
         self.wait_time = wait_time

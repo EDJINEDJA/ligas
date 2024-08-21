c988e231cc59a988ebe8bb7787e2a65abd2bad56
ArafatePro <91158220+ArafatePro@users.noreply.github.com>
2024-08-21 19:44:54 +0200

    Adding fixtures

--- test/unit_test.py
+++ test/unit_test.py
@@ -20,14 +20,6 @@ class testLigasfbrefApi(unittest.TestCase):
 
         self.assertIsInstance(response , requests.Response)
     
-<<<<<<< HEAD
-    def test_invalid_key_word_of_get_current_seasons(self):
-        with pytest.raises(Exception) as e_info:
-            response = self.api.get_valid_seasons(league = "La-ligas")
-
-        
-    def test_get_current_seasons(self):
-=======
     # def test_invalid_key_word_of_get_current_seasons(self):
 
     #     response = self.api.get_valid_seasons(league = "La-ligas")
@@ -35,24 +27,10 @@ class testLigasfbrefApi(unittest.TestCase):
     #     self.assertIsInstance(response , SeasonUrls)
 
     # def test_get_current_seasons(self):
->>>>>>> 2ec45bade752633b0a984cb09bdf64fc5fa10bcc
 
     #     response = self.api.get_valid_seasons(league = 'Serie A')
 
     #     self.assertIsInstance(response , SeasonUrls)
-    
-    def test_get_top_scorers(self):
-
-        response = self.api.get_top_scorers(league = 'Serie A')
-
-        self.assertIsInstance(response , dict)
-
-    def test_topScorer(self):
-
-        response = self.api.topScorer(league = 'Serie A',  currentSeason =  '2023-2024')
-
-        self.assertIsInstance(response , dict)
-
     def test_get_top_scorers(self):
 
         response = self.api.TopScorers(league = 'Serie A')
